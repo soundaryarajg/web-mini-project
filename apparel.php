9fc75fe8597aff979f6643859033797c7131d95a
soundaryarajg <71889436+soundaryarajg@users.noreply.github.com>
2020-12-09 13:54:00 +0530

    Add files via upload

---
+++ apparel.php
@@ -0,0 +1,351 @@
+<?php
+    session_start();
+    require 'check_if_added.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Lifestyle Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+	<style>
+	body{
+			background-attachment:fixed;
+			background-size:cover;
+		}
+		</style>
+    <body background="img/bg1.jpg">
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                <div class="jumbotron">
+                    <h1>Welcome to our LifeStyle Store!</h1>
+                    <p>We have the best cameras, watches and shirts for you. No need to hunt around, we have all in one place.</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/indiamart.jpg" alt="PN Rao">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>PN Rao</h3>
+                                    <p>Price: Rs. 4900.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(34)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=34 && bd=3" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                    
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/images3.jpg" alt="Khadi">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>khadi</h3>
+                                    <p>Price: Rs. 3400.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(33)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=33 && bd=3" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/images2.jpg" alt="Indiamart">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Indiamart</h3>
+                                    <p>Price: Rs. 5200.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(32)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=32 && bd=3" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/same.jpg" alt="Lifestyle">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Lifestyle</h3>
+                                    <p>Price: Rs. 4120.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(31)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=31 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/pinterest.jpg" alt="Westside">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Westside</h3>
+                                    <p>Price: Rs. 1980.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(30)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=30 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/myntra.jpg" alt="Myntra">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Myntra</h3>
+                                    <p>Price: Rs. 2800.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(29)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=29 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/images1.JPG" alt="Soch">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Soch</h3>
+                                    <p>Price: Rs. 3900.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(28)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=28 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/images.jpg" alt="Fab India">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Fab India</h3>
+                                    <p>Price: Rs. 5500.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(27)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=27 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/limeroad.jpg" alt="Mochi">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Mochi</h3>
+                                    <p>Price: Rs. 1500.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(9)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=9 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/mojari.jpg" alt="Mojari">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Mojari</h3>
+                                    <p>Price: Rs. 4200.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/mojari1.jpg" alt="Fabindia">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Fabindia</h3>
+                                    <p>Price: Rs. 2900.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(11)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=11 && bd=3" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+ 
+                </div>
+            </div>
+            <br><br><br><br><br><br><br><br>
+           <footer class="footer">
+            
+           </footer>
+        </div>
+    </body>
+</html>
---
+++ apple.php
@@ -0,0 +1,78 @@
+<?php
+    session_start();
+    require 'check_if_added.php';
+    require 'connection.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Herbs Shopping</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+	<style>
+	body{
+			background-attachment:fixed;
+			background-size:cover;
+		}
+		</style>
+    <body background="img/back.jpg">
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+
+<div class="col-md-3 col-sm-6" style="position: absolute; left:600px;">
+                        <div class="thumbnail">
+                        <img  src="img/apple.jpg" alt="Apple">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>Apple</h3>
+                                    <p>Price: Rs. 230.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(22)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=22 && bd=1" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+
+
+
+
+ <p style="background: white; position: relative; top:550px; text-align: center; padding: 100px; border-radius: 10px; font-size: large;" >
+ Apples are one of the most popular fruits — and for good reason.
+
+They’re an exceptionally healthy fruit with many research-backed benefits.
+
+Here are 10 impressive health benefits of apples.
+A medium apple — with a diameter of about 3 inches (7.6 centimeters) — equals 1.5 cups of fruit. Two cups of fruit daily are recommended on a 2,000-calorie diet.
+Apples are also a rich source of polyphenols. While nutrition labels don’t list these plant compounds, they’re likely responsible for many of the health benefits.
+
+To get the most out of apples, leave the skin on — it contains half of the fiber and many of the polyphenols.
+</p>
+
+</div>
+    </body>
+</html>
